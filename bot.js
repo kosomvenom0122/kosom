b0eb18cd777b6a2c7e7aef4cfc941f57ebee838d
kosomvenom0122 <46011033+kosomvenom0122@users.noreply.github.com>
2018-12-20 02:38:03 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("524326948906991639
+
+")
+setInterval(function() {
+channel.send(`ashalalasdklnasdnklasdkladak;lsdasasopd;ugaiygauifyaiufha;ojidfihd'lakfh'jahfjkahfa;klgjdljkcfbn;ljvbo'njfipoujitpsgayyjhiasdgk;dhojadfholsgpsgjklshkjhfkajsgdhjhagfkljaf;hgadghafjfgfaoglajhg'adogsho'odguifahuffa'udofgadfjkagfjkagfjklha'klfha'lkdhj'lkdfhslkhklsghskjg;fjgak'sfaklsghfkuadfa'dkfaklhdj'lfagh'
+djlfh'adlk`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
